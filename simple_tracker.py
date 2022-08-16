3bd14e7776b048f3536934c7ecbc680cc187c91b
Lasabi Olalekan <84998804+Larshabi@users.noreply.github.com>
2022-08-16 19:12:12 +0100

    last commit

--- simple_tracker.py
+++ simple_tracker.py
@@ -4,11 +4,40 @@ from selenium.common.exceptions import NoSuchElementException
 from datetime import datetime
 from amazon_config import get_chrome_web_driver, get_web_driver_options, set_automation_as_head_less, set_browser_as_incognito, set_ignore_certificate_error, DIRECTORY, NAME, CURRENCY, FILTERS, BASE_URL
 from selenium.webdriver.common.by import By
-
+import json
 
 class GenerateReport:
-    def __init__(self):
-        pass
+    def __init__(self, file_name, filters, base_link, currency, data):
+        self.data = data
+        self.file_name= file_name
+        self.filters = filters
+        self.base_link = base_link
+        self.currency = currency
+        report = {
+            'title':self.file_name,
+            'date':self.get_now(),
+            'best_item':self.get_best_item(),
+            'currency':self.currency,
+            'filters':self.filters,
+            'base_link':self.base_link,
+            'products':self.data
+        }
+        print("Creating report...")
+        with open(f'{DIRECTORY}/{file_name}.json', 'w') as f:
+            json.dump(report, f)
+            print("Done...")
+        
+    @staticmethod
+    def get_now():
+        now = datetime.now()
+        return now.strftime("%d/%m/Y %H:%M:%S")
+    
+    def get_best_item(self):
+        try:
+            return sorted(self.data, key=lambda k:k['price'])[0]
+        except Exception as e:
+            print(e)
+        
 
 class AmazonAPI:
     def __init__(self, search_item, filters, base_url, currency) -> None:
@@ -31,7 +60,9 @@ class AmazonAPI:
         print(f"Got {len(links)} links to the products")
         print("Getting info about products")
         products = self.get_product_info(links)
-        return None
+        print(f"Got info about {len(products)} products")
+        self.driver.quit()
+        return products
         
     def get_product_info(self, links):
         asins = self.get_asins(links)
@@ -71,10 +102,30 @@ class AmazonAPI:
         pass
     
     def get_seller(self):
-        pass
+        try:
+            self.driver.find_element(By.ID, 'bylineInfo').text
+        except Exception as e:
+            print(f"Can't get seller of a product - {self.driver.current_url}")
+            print(e)
+            return None
     
     def get_price(self):
-        pass
+        price = None
+        try:
+            price = self.driver.find_element(By.ID, "priceblock_ourprice").text
+        except NoSuchElementException as e:
+            try:
+                availability = self.driver.find_element(By.ID, 'availability').text
+                if 'Available' in availability:
+                    price = self.driver.find_element(By.CLASS_NAME, 'olp-padding-right').text
+            except Exception as e:
+                print(e)
+                return None
+        except Exception as e:
+            print(e)
+        return price
+            
+        
         
     def get_shorten_url(self, asin):
         return self.base_url + 'dp/' + asin
@@ -104,4 +155,6 @@ class AmazonAPI:
 if __name__ == '__main__':
     am = AmazonAPI(NAME, FILTERS, BASE_URL, CURRENCY)
     data = am.run()
+    GenerateReport(NAME, FILTERS, BASE_URL, CURRENCY, data)
+    
     
